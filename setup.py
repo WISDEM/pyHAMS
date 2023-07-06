833d2c7ae4f429eaf48e5cfd9884a5798571e257
Garrett Barter <garrett.barter@nrel.gov>
2023-07-06 03:43:08 -0600

    move libraries instead of copy them

--- setup.py
+++ setup.py
@@ -47,14 +47,12 @@ def run_meson_build(staging_dir):
     if meson_path is None:
         raise OSError("The meson command cannot be found on the system")
         
-    meson_call = (
-        f"{meson_path} setup {staging_dir} --wipe --prefix={prefix} "
-        + f"-Dpython.purelibdir={purelibdir} -Dpython.platlibdir={purelibdir} {meson_args}"
-    )
-    sysargs = meson_call.split(" ")
-    sysargs = [arg for arg in sysargs if arg != ""]
-    print(sysargs)
-    p1 = subprocess.run(sysargs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    meson_call = [meson_path, "setup", staging_dir, "--wipe",
+                  f"--prefix={prefix}", f"-Dpython.purelibdir={purelibdir}",
+                  f"-Dpython.platlibdir={purelibdir}", meson_args]
+    meson_call = [m for m in meson_call if m != ""]
+    print(meson_call)
+    p1 = subprocess.run(meson_call, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     os.makedirs(staging_dir, exist_ok=True)
     setup_log = os.path.join(staging_dir, "setup.log")
     with open(setup_log, "wb") as f:
@@ -62,23 +60,20 @@ def run_meson_build(staging_dir):
     if p1.returncode != 0:
         with open(setup_log, "r") as f:
             print(f.read())
-        raise OSError(sysargs, f"The meson setup command failed! Check the log at {setup_log} for more information.")
+        raise OSError(meson_call, f"The meson setup command failed! Check the log at {setup_log} for more information.")
 
     # build
-    meson_call = f"{meson_path} compile -vC {staging_dir}"
-    sysargs = meson_call.split(" ")
-    sysargs = [arg for arg in sysargs if arg != ""]
-    print(sysargs)
-    p2 = subprocess.run(sysargs, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    meson_call = [meson_path, "compile", "-vC", staging_dir]
+    meson_call = [m for m in meson_call if m != ""]
+    print(meson_call)
+    p2 = subprocess.run(meson_call, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     compile_log = os.path.join(staging_dir, "compile.log")
     with open(compile_log, "wb") as f:
         f.write(p2.stdout)
     if p2.returncode != 0:
         with open(compile_log, "r") as f:
             print(f.read())
-        raise OSError(
-            sysargs, f"The meson compile command failed! Check the log at {compile_log} for more information."
-        )
+        raise OSError(meson_call, f"The meson compile command failed! Check the log at {compile_log} for more information.")
 
 
 def copy_shared_libraries():
@@ -94,7 +89,7 @@ def copy_shared_libraries():
                 match = re.search(staging_dir, new_path)
                 new_path = new_path[match.span()[1] + 1 :]
                 print(f"Copying build file {file_path} -> {new_path}")
-                shutil.copy(file_path, new_path)
+                shutil.move(file_path, new_path)
 
 
 if __name__ == "__main__":
@@ -102,18 +97,12 @@ if __name__ == "__main__":
     # used as the sources for setuptools
     staging_dir = "meson_build"
 
-    # If on Windows, use the compiled exe from Yingyi
+    # this keeps the meson build system from running more than once
     if "dist" not in str(os.path.abspath(__file__)):
         cwd = os.getcwd()
         run_meson_build(staging_dir)
         os.chdir(cwd)
         copy_shared_libraries()
-
-    #docs_require = ""
-    #req_txt = os.path.join("doc", "requirements.txt")
-    #if os.path.isfile(req_txt):
-    #    with open(req_txt) as f:
-    #        docs_require = f.read().splitlines()
 
     init_file = os.path.join("pyhams", "__init__.py")
     #__version__ = re.findall(
